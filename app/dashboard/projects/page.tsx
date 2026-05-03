796e920eb0b2d132f9c51ab6697a9a79e32dbd1e
Takeru Hiroshi <yazeedkuantan@gmail.com>
2026-05-03 18:48:06 +0200

    FIXED Responsivness for project overview page

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -82,7 +82,7 @@ export default function Projects() {
     return (
         <div className="p-6">
             <h1 className="text-3xl font-bold text-[#172b4d]">Projects</h1>
-            <h2 className="text-lg mt-2 ml-6 font-semibold text-gray-500">List of projects</h2>
+            <h2 className="text-lg mt-2 md:ml-6 lg:ml-6 font-semibold text-gray-700">List of projects</h2>
             <div className="mt-6 ml-6 grid gap-4  md:grid-cols-2 lg:grid-cols-4">
                 {projects.map(project => (
                     <Link
