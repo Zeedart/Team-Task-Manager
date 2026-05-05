dadfc930f126e1f06b3e1cea9e4b12a75f2885e1
Takeru Hiroshi <yazeedkuantan@gmail.com>
2026-05-05 14:54:47 +0200

    Removed border from login/signup

--- components/auth/auth.tsx
+++ components/auth/auth.tsx
@@ -5,7 +5,7 @@ import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
 
 const Auth = () => {
     return (
-        <Tabs defaultValue="login" className="flex border border-[#dfe1e6] flex-col items-center justify-center">
+        <Tabs defaultValue="login" className="flex flex-col items-center justify-center">
             <TabsList className="bg-[#e8e9ec] w-full">
                 <TabsTrigger className="font-semibold" value="login">Login</TabsTrigger>
                 <TabsTrigger className="font-semibold" value="signup">Signup</TabsTrigger>
